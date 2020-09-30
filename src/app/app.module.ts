b6e65777f62a81f80204472ce863fec70d4da701
mdresch <m.drescher@cbaconsultancy.nl>
2020-09-30 08:05:11 +0200

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { HeroProfileComponent } from './hero-profile.component';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HeroProfileComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
